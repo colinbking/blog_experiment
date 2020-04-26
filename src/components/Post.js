83d8567f4e257eaacc9e66a4aaf630e8ace082f5
Colin King <cbk1@rice.edu>
2020-04-26 16:38:14 -0500

    added ordering of posts by most recent using graphql query

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,24 +1,35 @@
-import React from "react"
-import { Link } from "gatsby"
-import { Card, CardTitle, CardText, CardSubtitle, CardBody} from 'reactstrap'
 
-const Post = ({ title, author, path, date, body }) => {
-        return(
-                <Card>
-                        <CardBody>
-                                <CardTitle>{title}</CardTitle>
-                                <CardSubtitle>
-                                        <span className ="text-info">{date}</span> by{' '}
-                                        <span className ="author">{author}</span>  
-                                </CardSubtitle>
-                                <CardText>{body}</CardText>
-                                <Link to={path} className = "btn btn-outline-primary float-right">
-                                        Read more
-                                </Link>
+import React from 'react'
+import {
+  Badge,
+  Card,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  CardBody,
+} from 'reactstrap'
+import { Link } from 'gatsby'
+// import Img from 'gatsby-image'
+// import { slugify } from '../util/utilityFunctions'
 
-                        </CardBody>
-                </Card>
-        )
-}
-
+const Post = ({ title, author, path, date, body}) => (
+        <Card>
+          <CardBody>
+            <CardTitle>
+              <Link> {title}</Link>
+            </CardTitle>
+            <CardSubtitle>
+              <span className="text-info">{date}</span> by{' '}
+              <span className="text-info">{author}</span>
+            </CardSubtitle>
+            <CardText>{body}</CardText>
+            <Link
+              to={path}
+              className="btn btn-outline-primary float-right text-uppercase"
+            >
+              Read more
+            </Link>
+          </CardBody>
+        </Card>
+      )
 export default Post
