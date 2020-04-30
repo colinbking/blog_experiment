694849d87e9e180a5443ae5a4d17629e48644a67
Colin King <cbk1@rice.edu>
2020-04-30 02:02:00 -0500

    created tag page

---
+++ src/templates/post-list.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Post from '../components/Post'
+import { graphql } from 'gatsby'
+
+const postList = () => {
+
+}
+
+export const postListQuery = graohql`
+        
+`
+
+export default postList
